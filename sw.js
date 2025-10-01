b256d33382e43edea58f52f1332ac6297686d432
grundenschema <grundenschema@gmail.com>
2025-10-01 20:16:30 +0200

    Update and rename sw.js to xxx

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", event => {
-  console.log("Service worker installerad");
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
